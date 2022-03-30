9b14063ad0cc3259c180d114d455464a9e254eb4
Arnaud Juracek <arnaud.juracek@gmail.com>
2022-03-30 16:17:09 +0200

    Improve accessibility

--- www/site/snippets/components/Menu.php
+++ www/site/snippets/components/Menu.php
@@ -1,7 +1,7 @@
 <?php $class = $class ?? '' ?>
 
 <header class='menu <?= $class ?>' role='main'>
-  <a href='<?= $page->isHomePage() ? '#top' : $site->url() ?>'><?php snippet('svg/nut') ?></a>
+  <a href='<?= $page->isHomePage() ? '#top' : $site->url() ?>'><?php snippet('svg/nut', ['title' => $site->title()]) ?></a>
   <div class='flexgroup'>
     <a href='<?= $page->isHomePage() ? '' : $site->url() ?>#contact'>contact</a>
   </div>
